bc437b8569c37cc461517bc923e2839d1b0b5fb4
Jon <jonathon.lockwood@networkedweb.com>
2024-08-15 13:18:25 +0930

    fix: rtn ver to gal file

--- .github/additional_actions_bump.sh
+++ .github/additional_actions_bump.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-sed -E "s/version: (.+)/version: ${NEW_VERSION}/g" -i galaxy.yml
+echo "[Debug] script - NEW_VERSION=${NEW_VERSION}";
+
+sed -E "s/version: (.+)/version: ${NEW_VERSION}/g" -i galaxy.yml;
